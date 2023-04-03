333f37c832318c706ded6a41288425768facb72f
Rodrigo Monteiro <rodrigo@caprica.com.br>
2023-04-03 21:43:12 +0000

    Added double quotes to variable name in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 # Declare project variables here
 
-variable environment {
+variable "environment" {
     description = "Environment type for a specif scenario, dev, stage or prod"
     type = string
     default = "development"
