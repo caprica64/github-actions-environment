10c2de2b1d09783f228da223cf46741b3e1661a2
Rodrigo Monteiro <rodrigo@caprica.com.br>
2023-04-04 20:38:54 +0000

    mmmmmm

--- main.tf
+++ main.tf
@@ -1,28 +1,40 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "4.52.0"
+#     }
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "3.4.3"
+#     }
+#   }
+#   required_version = ">= 1.1.0"
+# 
+#   cloud {
+#     organization = "caprica"
+# 
+#     workspaces {
+#       name = "GitHub-Actions-Environments-dev"
+#     }
+#   }
+# }
 
-  cloud {
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
     organization = "caprica"
 
     workspaces {
-      #name = "GitHub-Actions-Environments-dev"
       prefix = "GitHub-Actions-Environments-"
     }
   }
 }
+
+
 
 provider "aws" {
   region = "us-east-1"
